d070123c34c92ba4734063568f58972f2ea22910
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2024-04-01 14:26:54 +0100

    Add toggleterm.

--- init.lua
+++ init.lua
@@ -882,6 +882,34 @@ require('lazy').setup({
   { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
   { 'tpope/vim-fugitive' },
   { 'tpope/vim-vinegar' },
+  {
+    'akinsho/toggleterm.nvim',
+    version = 'v2.10.x',
+    config = function()
+      require('toggleterm').setup {
+        open_mapping = [[<C-t>]],
+        start_in_insert = true,
+        persist_mode = true,
+        direction = 'float',
+        close_on_exit = true,
+        auto_scroll = true,
+      }
+
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+      end
+
+      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+      vim.cmd 'autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()'
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
