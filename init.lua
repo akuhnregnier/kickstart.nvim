aadf99cdecf08e6fcdf5e51fefe2e39735a32163
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2024-04-01 18:00:27 +0100

    enable spellcheck

--- init.lua
+++ init.lua
@@ -963,5 +963,8 @@ require('lazy').setup({
   },
 })
 
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_gb' }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
