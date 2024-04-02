a2384e666eaabd0c6d0db6d9d8289b196982858c
Alexander Kuhn-Regnier <akhunregnier@landytech.com>
2024-04-02 11:43:43 +0100

    Use gl to view diagnostics.

--- init.lua
+++ init.lua
@@ -164,7 +164,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = '[L]SP show diagnostic [E]rror messages' })
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = '[L]SP show diagnostic error messages' })
 vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = '[L]SP open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
