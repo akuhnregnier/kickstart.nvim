e93a766e1f1ddacf758209d0919112de250c08c3
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2024-04-06 20:28:57 +0100

    add dial

--- init.lua
+++ init.lua
@@ -1273,6 +1273,67 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>b', '<cmd>:Neotree<cr>', { desc = 'Open file explorer' })
     end,
   },
+  {
+    'monaqa/dial.nvim',
+    config = function()
+      local augend = require 'dial.augend'
+      require('dial.config').augends:register_group {
+        -- default augends used when no group name is specified
+        default = {
+          augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+          augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+          augend.date.alias['%Y/%m/%d'], -- date (2022/02/19, etc.)
+          augend.date.alias['%d/%m/%Y'],
+          augend.date.alias['%m/%d'],
+          augend.date.alias['%H:%M:%S'],
+          augend.date.alias['%H:%M'],
+          augend.constant.alias.bool,
+          augend.constant.new {
+            elements = { 'True', 'False' },
+            word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+            cyclic = true, -- "or" is incremented into "and".
+          },
+          augend.constant.alias.alpha,
+          augend.constant.alias.Alpha,
+          augend.constant.new {
+            elements = { 'and', 'or' },
+            word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+            cyclic = true, -- "or" is incremented into "and".
+          },
+          augend.constant.new {
+            elements = { '&&', '||' },
+            word = false,
+            cyclic = true,
+          },
+          augend.semver.alias.semver,
+        },
+      }
+      vim.keymap.set('n', '<C-a>', function()
+        require('dial.map').manipulate('increment', 'normal')
+      end)
+      vim.keymap.set('n', '<C-x>', function()
+        require('dial.map').manipulate('decrement', 'normal')
+      end)
+      vim.keymap.set('n', 'g<C-a>', function()
+        require('dial.map').manipulate('increment', 'gnormal')
+      end)
+      vim.keymap.set('n', 'g<C-x>', function()
+        require('dial.map').manipulate('decrement', 'gnormal')
+      end)
+      vim.keymap.set('v', '<C-a>', function()
+        require('dial.map').manipulate('increment', 'visual')
+      end)
+      vim.keymap.set('v', '<C-x>', function()
+        require('dial.map').manipulate('decrement', 'visual')
+      end)
+      vim.keymap.set('v', 'g<C-a>', function()
+        require('dial.map').manipulate('increment', 'gvisual')
+      end)
+      vim.keymap.set('v', 'g<C-x>', function()
+        require('dial.map').manipulate('decrement', 'gvisual')
+      end)
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
