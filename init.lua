a0d69b09f44c1ee81889d73dae75db79f7def647
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2024-04-06 20:46:40 +0100

    Add lsp_signature.nvim

--- init.lua
+++ init.lua
@@ -1341,6 +1341,14 @@ require('lazy').setup({
       dim = true,
     },
   },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
